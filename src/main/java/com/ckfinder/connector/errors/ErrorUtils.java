3fba8615288a11cfa3de1261d05e625b9226446e
zhanhb <6323014+zhanhb@users.noreply.github.com>
2016-06-05 21:46:42 +0800

    ResourceBundle instead of xml parsing.

--- src/main/java/com/ckfinder/connector/errors/ErrorUtils.java
+++ src/main/java/com/ckfinder/connector/errors/ErrorUtils.java
@@ -11,27 +11,9 @@
  */
 package com.ckfinder.connector.errors;
 
-import com.ckfinder.connector.ConnectorServlet;
-import com.ckfinder.connector.configuration.Constants;
 import com.ckfinder.connector.configuration.IConfiguration;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * Error utils.
@@ -39,8 +21,6 @@ import org.w3c.dom.NodeList;
 public final class ErrorUtils {
 
     private static final ErrorUtils errorUtils = new ErrorUtils();
-
-    private static Map<String, Map<Integer, String>> langMap;
 
     /**
      * Returns or creates and returns {@code ErrorUtils} instance.
@@ -56,9 +36,6 @@ public final class ErrorUtils {
      * initializes list of available languages supported by CKFinder.
      */
     private ErrorUtils() {
-        langMap = getLangCodeFromJars().stream()
-                .collect(Collectors.toMap(Function.identity(),
-                        this::getMessagesByLangCode));
     }
 
     /**
@@ -72,65 +49,13 @@ public final class ErrorUtils {
     public String getErrorMsgByLangAndCode(final String lang,
             final int errorCode,
             final IConfiguration conf) {
-        if (lang != null && langMap.get(lang) != null) {
-            return langMap.get(lang).get(errorCode);
-        } else if (langMap.get(Constants.DEFAULT_LANG_CODE) != null) {
-            return langMap.get(Constants.DEFAULT_LANG_CODE).get(errorCode);
-        } else if (conf.isDebugMode()) {
-            return "Unable to load error message";
-        } else {
-            return "";
-        }
-    }
-
-    /**
-     * Gets language codes from jar file holding locale files.
-     *
-     * @return {@code List} with language codes.
-     */
-    private List<String> getLangCodeFromJars() {
-        List<String> langFiles = new ArrayList<>();
         try {
-            Path dir = Paths.get(ConnectorServlet.class.getResource("/lang/").toURI());
-            if (Files.isDirectory(dir)) {
-                for (Iterator<Path> it = Files.newDirectoryStream(dir).iterator(); it.hasNext();) {
-                    langFiles.add(it.next().getFileName().toString().replaceAll(".xml", ""));
-                }
-            }
-        } catch (URISyntaxException | IOException ex) {
-            return null;
+            return ResourceBundle.getBundle(ErrorUtils.class.getPackage()
+                    .getName().concat(".LocalStrings"), new Locale(lang))
+                    .getString(Integer.toString(errorCode));
+        } catch (RuntimeException ex) {
+            return conf.isDebugMode() ? "Unable to load error message" : "";
         }
-        return langFiles;
-    }
-
-    /**
-     * Gets localized messages from locale file by language code provided as
-     * parameter.
-     *
-     * @param langCode language code used to get localized messages
-     * @return {@code Map} of localized messages.
-     */
-    private Map<Integer, String> getMessagesByLangCode(final String langCode) {
-        Map<Integer, String> langCodeMap = new HashMap<>();
-        try {
-            InputStream is = ConnectorServlet.class.getResourceAsStream("/lang/" + langCode + ".xml");
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse(is);
-            NodeList unkonwErrornodeList = doc.getElementsByTagName("errorUnknown");
-            NodeList errorNodeList = doc.getElementsByTagName("error");
-            Element unkonwErrorElem = (Element) unkonwErrornodeList.item(0);
-            langCodeMap.put(1, unkonwErrorElem.getTextContent());
-
-            for (int i = 0, j = errorNodeList.getLength(); i < j; i++) {
-                Element element = (Element) errorNodeList.item(i);
-                langCodeMap.put(Integer.valueOf(element.getAttribute("number")),
-                        element.getTextContent());
-            }
-        } catch (Exception e) {
-            return null;
-        }
-        return langCodeMap;
     }
 
 }
