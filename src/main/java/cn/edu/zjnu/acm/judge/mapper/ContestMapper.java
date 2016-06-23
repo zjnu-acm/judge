eaafff0d9836a408b4dc326e4540b404496d530f
zhanhb <6323014+zhanhb@users.noreply.github.com>
2016-06-23 20:12:42 +0800

    Organize imports.

--- src/main/java/cn/edu/zjnu/acm/judge/mapper/ContestMapper.java
+++ src/main/java/cn/edu/zjnu/acm/judge/mapper/ContestMapper.java
@@ -28,7 +28,6 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectKey;
 import org.apache.ibatis.annotations.Update;
-import org.apache.ibatis.binding.BindingException;
 
 /**
  *
