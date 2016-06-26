30f9709bcdc57c7413028d075231883f187ecb4b
zhanhb <6323014+zhanhb@users.noreply.github.com>
2016-06-26 19:22:37 +0800

    Manage contest problem add.

--- src/main/webapp/js/site.js
+++ src/main/webapp/js/site.js
@@ -5,7 +5,7 @@ jQuery(function ($) {
         this.start && this.start();
     });
     $("[data-lang]").each(function () {
-        var s = "?lang=" + $(this).data("lang"), t = location.search.substr(1);
-        $(this).attr("href", t ? s + "&" + t.replace(/lang(=[^&]*)?($|&)/g,"") : s);
+        var s = "?lang=" + $(this).data("lang"), t = location.search.substr(1).replace(/(^|&)lang(=[^&]*)?($|(?=&))/g, "").replace(/^&+/, "");
+        $(this).attr("href", t ? s + "&" + t : s);
     });
 });
