f4b4bb5fa458ba05f59ba5703b95cdc718c7123a
zhanhb <zhanhb88@gmail.com>
2016-07-27 13:29:53 +0800

    Polish

--- src/main/java/cn/edu/zjnu/acm/judge/bbs/BBSController.java
+++ src/main/java/cn/edu/zjnu/acm/judge/bbs/BBSController.java
@@ -2,7 +2,6 @@ package cn.edu.zjnu.acm.judge.bbs;
 
 import cn.edu.zjnu.acm.judge.domain.Message;
 import cn.edu.zjnu.acm.judge.mapper.MessageMapper;
-import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -26,12 +25,11 @@ public class BBSController {
     @GetMapping(value = "/bbs", produces = TEXT_HTML_VALUE)
     protected ResponseEntity<String> bbs(
             @RequestParam(value = "problem_id", required = false) Long problemId,
-            @RequestParam(value = "size", defaultValue = "50") int threadLimit,
-            @RequestParam(value = "top", defaultValue = "99999999") long top) throws SQLException {
-        threadLimit = Math.min(threadLimit, 500);
-        threadLimit = Math.max(threadLimit, 0);
+            @RequestParam(value = "size", defaultValue = "50") final int threadLimit,
+            @RequestParam(value = "top", defaultValue = "99999999") final long top) {
+        int limit = Math.max(Math.min(threadLimit, 500), 0);
 
-        final long mint = messageMapper.mint(top, problemId, threadLimit, 0);
+        final long mint = messageMapper.mint(top, problemId, limit, 0);
         final List<Message> messages = messageMapper.findAllByThreadIdBetween(mint, top, problemId, null);
 
         long currentDepth = 0;
@@ -39,8 +37,8 @@ public class BBSController {
 
         StringBuilder sb = new StringBuilder("<html><head><title>Messages</title></head><body>"
                 + "<table border=0 width=100% class=table-back><tr><td><ul>");
-        top = messages.stream().mapToLong(Message::getThread).max().orElse(0);
-        final long maxt = messageMapper.maxt(top, problemId, threadLimit, 999999999999L);
+        long maxThreadId = messages.stream().mapToLong(Message::getThread).max().orElse(0);
+        final long maxt = messageMapper.maxt(maxThreadId, problemId, limit, 999999999999L);
         for (Message message : messages) {
             long depth = message.getDepth();
             String title = message.getTitle();
@@ -60,9 +58,9 @@ public class BBSController {
                 sb.append("<hr/>");
             }
             lastThreadId = threadId;
-            sb.append("<li><a href=showmessage?message_id=").append(messageId).append("><font color=blue>").append(StringUtils.escapeXml(title)).append("</font></a> <b><a href=userstatus?user_id=").append(userId).append("><font color=black>").append(userId).append("</font></a></b> ").append(timestamp);
+            sb.append("<li><a href=\"showmessage?message_id=").append(messageId).append("\"><font color=blue>").append(StringUtils.escapeXml(title)).append("</font></a> <b><a href=\"userstatus?user_id=").append(userId).append("\"><font color=black>").append(userId).append("</font></a></b> ").append(timestamp);
             if (problem != null && problem != 0L && depth == 0) {
-                sb.append(" <b><a href=showproblem?problem_id=").append(problem).append("><font color=#000>Problem ").append(problem).append("</font></a></b>");
+                sb.append(" <b><a href=\"showproblem?problem_id=").append(problem).append("\"><font color=#000>Problem ").append(problem).append("</font></a></b>");
             }
         }
         for (; currentDepth > 0; currentDepth--) {
@@ -73,21 +71,21 @@ public class BBSController {
         if (problemId != null) {
             query = "?problem_id=" + problemId;
         }
-        sb.append("<hr/>[<a href=bbs").append(query).append(">Top</a>]");
+        sb.append("<hr/>[<a href=\"").append(query).append("\">Top</a>]");
         query = "?top=" + maxt;
         if (problemId != null) {
             query = query + "&problem_id=" + problemId;
         }
-        sb.append("&nbsp;&nbsp;&nbsp;[<a href=bbs").append(query).append(">Previous</a>]");
+        sb.append("&nbsp;&nbsp;&nbsp;[<a href=\"").append(query).append("\">Previous</a>]");
         query = "?top=" + mint;
         if (problemId != null) {
             query = query + "&problem_id=" + problemId;
         }
-        sb.append("&nbsp;&nbsp;&nbsp;[<a href=bbs").append(query).append(">Next</a>]<br/></center><form action=postpage>");
+        sb.append("&nbsp;&nbsp;&nbsp;[<a href=\"").append(query).append("\">Next</a>]<br/></center><form action=postpage>");
         if (problemId != null) {
-            sb.append("<input type=hidden name=problem_id value=").append(problemId).append(">");
+            sb.append("<input type=\"hidden\" name=\"problem_id\" value=\"").append(problemId).append("\">");
         }
-        sb.append("<button type=submit>Post new message</button></form></body></html>");
+        sb.append("<button type=\"submit\">Post new message</button></form></body></html>");
         return ResponseEntity.ok().contentType(MediaType.valueOf("text/html;charset=UTF-8")).body(sb.toString());
     }
 
