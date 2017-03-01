0f210442d332ab2ce408f8066ad2eb8ba0fed564
zhanhb <6323014+zhanhb@users.noreply.github.com>
2017-03-01 23:46:55 +0800

    Get language name directly in show source page

--- src/main/java/cn/edu/zjnu/acm/judge/submission/ShowSourceController.java
+++ src/main/java/cn/edu/zjnu/acm/judge/submission/ShowSourceController.java
@@ -44,7 +44,7 @@ public class ShowSourceController {
         if (!submissionService.canView(request, submission)) {
             throw new MessageException("You have no permission to view the source.", HttpStatus.FORBIDDEN);
         }
-        String language = languageService.getAvailableLanguage(submission.getLanguage()).getName();
+        String language = languageService.getLanguageName(submission.getLanguage());
 
         if (style == null) {
             style = userPerferenceMapper.getStyle(userId);
