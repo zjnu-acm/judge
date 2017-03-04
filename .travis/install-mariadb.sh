7c751130bc95802be1ed1001e352e61081881d6e
zhanhb <6323014+zhanhb@users.noreply.github.com>
2017-03-05 00:39:16 +0800

    Upgrade to mariadb 10.2

--- .travis/install-mariadb.sh
+++ .travis/install-mariadb.sh
@@ -13,7 +13,7 @@ service mysql stop
 
 apt-get install python-software-properties
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-add-apt-repository 'deb [arch=amd64,i386] http://mirrors.accretive-networks.net/mariadb/repo/10.1/ubuntu precise main'
+add-apt-repository 'deb [arch=amd64,i386] http://mirrors.accretive-networks.net/mariadb/repo/10.2/ubuntu precise main'
 apt-get update
 apt-get install mariadb-server
 
