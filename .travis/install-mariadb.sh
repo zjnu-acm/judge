dfa559ab4c8a5c65fa41573b90b8339f9aaf531b
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-04-30 11:05:34 +0800

    Upgrade to mariadb 10.3 on travis

--- .travis/install-mariadb.sh
+++ .travis/install-mariadb.sh
@@ -11,9 +11,9 @@ remove_mysql
 
 service mysql stop
 
-apt-get install python-software-properties
+apt-get install software-properties-common
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-add-apt-repository 'deb [arch=amd64,i386] http://mirrors.accretive-networks.net/mariadb/repo/10.2/ubuntu precise main'
+add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mariadb.mirror.iweb.com/repo/10.3/ubuntu trusty main'
 apt-get update
 apt-get install mariadb-server
 
