0948b6563429b64f3d79039884d6a76359798b02
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-05-03 22:48:18 +0800

    Merge class ProblemListController SearchProblemController

--- src/main/java/cn/edu/zjnu/acm/judge/controller/problem/ProblemListController.java
+++ src/main/java/cn/edu/zjnu/acm/judge/controller/problem/ProblemListController.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2019 ZJNU ACM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.edu.zjnu.acm.judge.controller.problem;
 
 import cn.edu.zjnu.acm.judge.data.form.ProblemForm;
 import cn.edu.zjnu.acm.judge.domain.Problem;
+import cn.edu.zjnu.acm.judge.exception.BusinessCode;
+import cn.edu.zjnu.acm.judge.exception.BusinessException;
 import cn.edu.zjnu.acm.judge.service.ProblemService;
 import cn.edu.zjnu.acm.judge.util.URIBuilder;
 import java.util.Locale;
@@ -13,6 +30,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -21,7 +39,7 @@ public class ProblemListController {
 
     private final ProblemService problemService;
 
-    @GetMapping({"/problemlist", "/problems"})
+    @GetMapping({"problemlist", "problems"})
     public String problemList(ProblemForm form, Model model, Locale locale, Authentication authentication,
             @PageableDefault(100) Pageable pageable, HttpServletRequest request) {
         String currentUserId = authentication != null ? authentication.getName() : null;
@@ -31,6 +49,22 @@ public class ProblemListController {
         Page<Problem> page = problemService.findAll(form, currentUserId, pageable, locale);
         model.addAttribute("page", page);
         return "problems/list";
+    }
+
+    @GetMapping("searchproblem")
+    public String searchProblem(ProblemForm form, Model model, Locale locale, Authentication authentication,
+                                @PageableDefault(1000) Pageable pageable, HttpServletRequest request) {
+        if (!StringUtils.hasText(form.getSstr())) {
+            throw new BusinessException(BusinessCode.PROBLEM_SEARCH_KEY_EMPTY);
+        }
+        String currentUserId = authentication != null ? authentication.getName() : null;
+        String url = URIBuilder.fromRequest(request).replaceQueryParam("page").toString();
+        model.addAttribute("url", url);
+        model.addAttribute("query", form.getSstr());
+        form.setDisabled(Boolean.FALSE);
+        Page<Problem> page = problemService.findAll(form, currentUserId, pageable, locale);
+        model.addAttribute("page", page);
+        return "problems/search";
     }
 
 }
--- src/test/java/cn/edu/zjnu/acm/judge/controller/problem/ProblemListControllerTest.java
+++ src/test/java/cn/edu/zjnu/acm/judge/controller/problem/ProblemListControllerTest.java
@@ -1,20 +1,40 @@
+/*
+ * Copyright 2019 ZJNU ACM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.edu.zjnu.acm.judge.controller.problem;
 
 import cn.edu.zjnu.acm.judge.Application;
+import cn.edu.zjnu.acm.judge.data.form.ProblemForm;
 import java.util.Locale;
 import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -44,6 +64,27 @@ public class ProblemListControllerTest {
         Boolean disabled = null;
         Locale locale = Locale.getDefault();
         MvcResult result = mvc.perform(get("/problemlist")
+                .param("sstr", sstr)
+                .param("disabled", Objects.toString(disabled, ""))
+                .locale(locale))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andReturn();
+    }
+
+    /**
+     * Test of searchProblem method, of class ProblemListController.
+     *
+     * @see ProblemListController#searchProblem(ProblemForm, Model, Locale,
+     * Authentication, Pageable, HttpServletRequest)
+     */
+    @Test
+    public void testSearchProblem() throws Exception {
+        log.info("searchProblem");
+        String sstr = "test";
+        Boolean disabled = null;
+        Locale locale = Locale.getDefault();
+        MvcResult result = mvc.perform(get("/searchproblem")
                 .param("sstr", sstr)
                 .param("disabled", Objects.toString(disabled, ""))
                 .locale(locale))
