1fa74a8b587d50df7ac9dc9c236562568acf67a1
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-05-05 22:12:57 +0800

    Rename test package a to main

--- src/test/java/main/ExtensionsViewer.java
+++ src/test/java/main/ExtensionsViewer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package a;
+package main;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
--- src/test/java/main/SourceVisitor.java
+++ src/test/java/main/SourceVisitor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package a;
+package main;
 
 import com.google.common.base.Strings;
 import java.io.BufferedReader;
