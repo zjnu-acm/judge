305ec746f8605afb39087d27d0bc6d4b77b01474
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-05-05 23:35:38 +0800

    Add scheduing task clean expired cookies

---
+++ src/main/java/cn/edu/zjnu/acm/judge/config/RememberMeCleaner.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 ZJNU ACM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.edu.zjnu.acm.judge.config;
+
+import cn.edu.zjnu.acm.judge.mapper.PersistentLoginMapper;
+import java.time.Duration;
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Slf4j
+@Configuration
+@EnableScheduling
+@RequiredArgsConstructor
+public class RememberMeCleaner {
+
+    private static final Duration DEFAULT_EXPIRE_TIMEOUT = Duration.ofDays(14);
+
+    private final PersistentLoginMapper persistentLoginMapper;
+
+    // at 2:54 everyday
+    @Scheduled(cron = "0 54 2 ? * *")
+    public void cleanUpExpiredCookies() {
+        Instant boundary = Instant.now().minus(DEFAULT_EXPIRE_TIMEOUT);
+        persistentLoginMapper.cleanUpExpiredCookies(boundary);
+    }
+
+}
---
+++ src/test/java/cn/edu/zjnu/acm/judge/config/RememberMeCleanerTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 ZJNU ACM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.edu.zjnu.acm.judge.config;
+
+import cn.edu.zjnu.acm.judge.Application;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author zhanhb
+ */
+@RunWith(SpringRunner.class)
+@Slf4j
+@SpringBootTest(classes = Application.class)
+@Transactional
+@WebAppConfiguration
+public class RememberMeCleanerTest {
+
+    @Autowired
+    private RememberMeCleaner instance;
+
+    /**
+     * Test of cleanUpExpiredCookies method, of class RememberMeCleaner.
+     */
+    @Test
+    public void testCleanUpExpiredCookies() {
+        log.info("cleanUpExpiredCookies");
+        instance.cleanUpExpiredCookies();
+    }
+
+}
