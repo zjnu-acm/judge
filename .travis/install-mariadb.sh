cf7a789f926360e235d84282395f0eec587f8290
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-05-20 01:18:23 +0800

    Polish

--- .travis/install-mariadb.sh
+++ .travis/install-mariadb.sh
@@ -9,8 +9,6 @@ remove_mysql(){
 
 remove_mysql
 
-service mysql stop
-
 apt-get install software-properties-common
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mariadb.mirror.iweb.com/repo/10.3/ubuntu trusty main'
