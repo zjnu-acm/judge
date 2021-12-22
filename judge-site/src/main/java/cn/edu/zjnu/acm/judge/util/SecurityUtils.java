0d7fb3088a74a8d161699ad33cf41fb6e3407cc9
zhanhb <6323014+zhanhb@users.noreply.github.com>
2021-12-23 03:11:32 +0800

    Fix user id getting
    got the username `anonymousUser` if not logged

--- judge-site/src/main/java/cn/edu/zjnu/acm/judge/util/SecurityUtils.java
+++ judge-site/src/main/java/cn/edu/zjnu/acm/judge/util/SecurityUtils.java
@@ -2,6 +2,7 @@ package cn.edu.zjnu.acm.judge.util;
 
 import java.util.Optional;
 import javax.annotation.Nullable;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -13,7 +14,10 @@ public class SecurityUtils {
 
     @Nullable
     public static String getUserId() {
-        return Optional.ofNullable(getAuthentication()).map(Authentication::getName).orElse(null);
+        return Optional.ofNullable(getAuthentication())
+                .filter(auth -> !AnonymousAuthenticationToken.class.isInstance(auth))
+                .map(Authentication::getName)
+                .orElse(null);
     }
 
 }
